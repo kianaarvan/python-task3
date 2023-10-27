f35f35be6d109bada2746f79d8b4863eaf32d14b
kianaarvan <148455828+kianaarvan@users.noreply.github.com>
2023-10-27 20:41:50 +0330

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,41 @@
+import math
+while True:
+   
+    op=input("Enter op(+ ,- , * , / , sin , cos , tan , cot , factorial , sqrt) or exit:")
+    if op=="exit":
+       break
+    a=float(input("enter a:"))
+    b=float(input("enter b:"))
+    x=float(input("enter x:"))
+
+    if op=="+":
+         a=float(input("enter a:"))
+         b=float(input("enter b:"))
+         r=a+b
+    if op=="-":
+        r=a-b
+    if op=="*":
+        r=a*b
+    if op=="/":
+        if b==0:
+            r="error"
+        else:
+            r=a/b   
+    if op=="sin":
+        r=(math.sin(math.radians(x)))
+    if op=="cos":
+        r=(math.cos(math.radians(x)))
+    if op=="tan":
+        r=(math.tan(math.radians(x)))
+    if op=="cot":
+        r=(1)/(math.tan(math.radians(x)))
+    if op=="factorial":
+        r=(math.factorial(x))
+    if op=="sqrt":
+        if x<0:
+            r="erorr!"
+        else:
+            r=(math.sqrt(x))
+            
+    print(r)
+
